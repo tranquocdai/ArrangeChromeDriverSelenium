7acd82ad34842520a8b43827a5ad2a01c3ccdca3
Tran Quoc Dai <mr.tranquocdai@gmail.com>
2021-07-01 01:40:59 +0700

    Add files via upload

---
+++ DaLuong/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace DaLuong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTN_Start = new System.Windows.Forms.Button();
+            this.Num_luong = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.Num_luong)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BTN_Start
+            // 
+            this.BTN_Start.Location = new System.Drawing.Point(236, 62);
+            this.BTN_Start.Name = "BTN_Start";
+            this.BTN_Start.Size = new System.Drawing.Size(123, 46);
+            this.BTN_Start.TabIndex = 0;
+            this.BTN_Start.Text = "START";
+            this.BTN_Start.UseVisualStyleBackColor = true;
+            this.BTN_Start.Click += new System.EventHandler(this.BTN_Start_Click);
+            // 
+            // Num_luong
+            // 
+            this.Num_luong.Location = new System.Drawing.Point(52, 77);
+            this.Num_luong.Name = "Num_luong";
+            this.Num_luong.Size = new System.Drawing.Size(76, 20);
+            this.Num_luong.TabIndex = 2;
+            this.Num_luong.Value = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 181);
+            this.Controls.Add(this.Num_luong);
+            this.Controls.Add(this.BTN_Start);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Num_luong)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTN_Start;
+        private System.Windows.Forms.NumericUpDown Num_luong;
+    }
+}
+
---
+++ DaLuong/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using OpenQA.Selenium.Chrome;
+
+namespace DaLuong
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Action()
+        {
+            int luong = (int)Num_luong.Value;
+            Rectangle workingArea = Screen.GetWorkingArea(this);
+            for (int i = 0; i < (luong/2); i++)
+            {
+                int x = 320*i;
+                for (int j = 0; j < 2; j++)
+                {
+                    int y = 480*j;
+                    Thread thread = new Thread((ThreadStart)delegate
+                    {
+                        ChromeOptions chromeOptions = new ChromeOptions();
+                        chromeOptions.BinaryLocation = "C:\\Program Files\\Google\\Chrome\\Application\\Chrome.exe";
+                        chromeOptions.AddArgument("--app=https://tranquocdai.com");
+                        chromeOptions.AddArgument("--window-size=320,480");
+                        chromeOptions.AddArgument("--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 14_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1");
+                        chromeOptions.AddExcludedArgument("enable-automation");
+                        chromeOptions.AddAdditionalCapability("useAutomationExtension", false);
+                        chromeOptions.AddUserProfilePreference("credentials_enable_service", false);
+                        chromeOptions.AddUserProfilePreference("profile.password_manager_enabled", false);
+                        ChromeDriverService chromeDriverService = ChromeDriverService.CreateDefaultService();
+                        chromeDriverService.HideCommandPromptWindow = true;
+                        ChromeDriver driver = new ChromeDriver(chromeDriverService, chromeOptions);
+
+                        driver.Manage().Window.Position = new Point(x,y);
+                        Thread.Sleep(1000);
+                    });
+                    thread.IsBackground = true;
+                    thread.Start();
+                }
+            }
+        }
+        private void BTN_Start_Click(object sender, EventArgs e)
+        {
+            if (BTN_Start.Text == "START")
+            {
+                BTN_Start.Text = "STOP";
+                Action();
+            }
+            else
+            {
+                BTN_Start.Text = "START";
+                try
+                {
+                    Process[] processesChrome = Process.GetProcessesByName("chromedriver");
+                    foreach (Process processChrome in processesChrome)
+                    {
+                        processChrome.Kill();
+                    }
+                }
+                catch { }
+            }
+        }
+    }
+}
